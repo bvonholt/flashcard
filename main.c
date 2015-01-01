425edbc927b4c3c84c20c87041734296c14c7f64
Benedict von Holtzendorff <bvonholt@uwo.ca>
2015-01-01 16:43:29 -0500

    first commit in branch linked-list-testing

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 #include "card.h"
 
 int main(void){
-	word_t *words = read_words();
-	for (int i = 0; i < sizeof words / sizeof(word_t); i++)
-		free(words[i].field
-	free(words);
 	return 0;
 }
 
